630137721dbf8ef9e01b591efe1ab80d305a0759
Knox S. Long <long@stsci.edu>
2024-01-25 15:40:50 -1000

    Updated the two scripts in chianti data to Python3
    (and made small modifications to get them to run
    on the current Chianty database.)
    
    But the coll_stren_lookup.py produces a very different
    result that what is currently stored, so either there
    was another program that was use to eliminate upsilons
    that were uninteresting or there is something that is
    currently being missted in the program.

--- chianti_data/coll_stren_lookup.py
+++ chianti_data/coll_stren_lookup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -i
+#!/usr/bin/env python 
 
 '''
                     Southampton University
@@ -26,16 +26,17 @@ Notes:
 History:
 
 19/7/12 NSH coding begun
+240125 - Updated to python3 and tested on current verions of chianti
 
 '''
 
 import os
 from types import *
-from ConfigParser import *
-import cPickle
+from configparser import *
+import pickle
 from datetime import date
 import numpy as np
-from FortranFormat import *
+# from FortranFormat import *
 from astropy import constants as c
 from astropy import units as u
 #import chianti.constants as const
@@ -74,7 +75,7 @@ iline=0
 np=0
 
 
-inp=open("lines_linked_ver_2.py","r'")
+inp=open("lines_linked_ver_2.dat","r")
 
 
 out=open("ver_upsilon_linked.dat","w")
@@ -83,57 +84,69 @@ coll_dat=[]
 SCT=[]
 SCUPS=[]
 
+jjj=0
 for line in inp.readlines():
-	data=line.split()
-	iline=iline+1
-	el=El[int(data[1])-1]
-	ion=el+'_'+data[2]
-	lamb=float(data[3])
-	g_l=int(data[5])
-	f=float(data[4])
-	nu=c.c.cgs/(lamb*u.angstrom).to(u.cm)
-	e=((c.h.cgs*nu).to(u.rydberg)).value
+    jjj+=1    
+    data=line.split()
+    iline=iline+1
+    try:
+        el=El[int(data[1])-1]
+        ion=el+'_'+data[2]
+        lamb=float(data[3])
+        g_l=int(data[5])
+        f=float(data[4])
+    except:
+        print('Error in lines_linkded %d %s' % (jjj,line))
+        continue
+    nu=c.c.cgs/(lamb*u.angstrom).to(u.cm)
+    e=((c.h.cgs*nu).to(u.rydberg)).value
 
 	
-	fname='Chianti/'+el+'/'+ion+'/'+ion+'.scups'
-	if os.path.isfile(fname) and f !=0:
-		input=open(fname,'r')
-		lines=input.readlines()
-		match=0
-		best_e=1e99
-		best_gf=1e99
-		best=-1
-		for i in range(0,len(lines),3):
-			data1=lines[i].split()
-			if int(data1[0])!=-1:
-				if int(data[5])>np:
-					np=int(data[5])
-				energy=float(data1[2])
-				gf=float(data1[3])
-				test_e=abs(energy-e)/e
-				test_gf=abs(gf-(g_l*f))/(g_l*f)
-				if test_e < tol and test_gf < tol:
-					if test_e<best_e and test_gf<best_gf:
-						best_e=test_e
-						best_gf=test_gf
-						match=1
-						best=i
-			else:
-				break
-		if match==1:
-			print "line ",iline,el,ion,lamb," has ",match," matches"
-			coll_dat.append(lines[best])
-			out.write("CSTREN "+line[:-1]+' '+lines[best])
-			out.write("SCT" +lines[best+1])
-			out.write("SCUPS "+lines[best+2])
-			SCT.append(lines[best+1])
-			SCUPS.append(lines[best+2])
-		elif match>1:
-			print "Uh oh, we have more than one match for ",data
-		else:
-			print "no match for ",data			
-	else:
-		print "line ",iline,el,ion,lamb," has no data in chianti"
+    fname='Chianti/'+el+'/'+ion+'/'+ion+'.scups'
+    if os.path.isfile(fname) and f !=0:
+        input=open(fname,'r')
+        lines=input.readlines()
+        match=0
+        best_e=1e99
+        best_gf=1e99
+        best=-1
+        for i in range(0,len(lines),3):
+            data1=lines[i].split()
+            try:
+                foo=int(data1[0])
+            except:
+                print('Error: on line %d of %s with %s' % (i,fname,lines[i]))
+                continue
+
+            if int(data1[0])!=-1:
+                if int(data[5])>np:
+                    np=int(data[5])
+                energy=float(data1[2])
+                gf=float(data1[3])
+                test_e=abs(energy-e)/e
+                test_gf=abs(gf-(g_l*f))/(g_l*f)
+                if test_e < tol and test_gf < tol:
+                    if test_e<best_e and test_gf<best_gf:
+                        best_e=test_e
+                        best_gf=test_gf
+                        match=1
+                        best=i
+                    else:
+                        break
+                if match==1:
+                    print("line ",iline,el,ion,lamb," has ",match," matches")
+                    coll_dat.append(lines[best])
+                    out.write("CSTREN "+line[:-1]+' '+lines[best])
+                    out.write("SCT" +lines[best+1])
+                    out.write("SCUPS "+lines[best+2])
+                    SCT.append(lines[best+1])
+                    SCUPS.append(lines[best+2])
+                elif match>1:
+                    print("Uh oh, we have more than one match for ",data)
+            else:
+                print("no match for ",data)			
+        else:
+            print("line ",iline,el,ion,lamb," has no data in chianti")
 		
 '''
         input.close()
@@ -225,5 +238,5 @@ out.close()
 	
 '''
 	
-print "fin"	
+print("fin")	
 
